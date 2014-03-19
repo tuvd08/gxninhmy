1cdd98f11313e3ec3f08e370dd3a1bf5f7eb213c
tu-vu-duy <tuvd@exoplatform.com>
2014-03-19 15:00:55 +0700

    Show Menu on mobile

--- wp-content/themes/twentythirteen/js/main-base.js
+++ wp-content/themes/twentythirteen/js/main-base.js
@@ -1,4 +1,27 @@
 (function($, window) {
-
+    //run before onload done
+    $('.on-show-menu').on('click', function() {
+      var menu = $('.menu-container');
+      var sMenu = $('.main_menu');
+      if(menu.css('display') === 'none') {//198x416
+        menu.show();
+        sMenu.css({width: '0px', height:'0px', overflow :'hidden'}).
+                  animate({width: '198px', height:'416px'}, 300, function() { $(this).removeAttr('style'); });
+      } else {
+        sMenu.css({overflow :'hidden'}).animate({width: '0px', height:'0px'}, 300, function() { 
+          $(this).removeAttr('style');
+          menu.hide();
+        });
+      }
+    });
+    
+    $('.on-hide-menu').on('click', function() {
+      var menu = $('.menu-container');
+      var sMenu = $('.main_menu');
+      sMenu.css({overflow :'hidden'}).animate({width: '0px', height:'0px'}, 300, function() { 
+          $(this).removeAttr('style');
+          menu.hide();
+      });
+    });
 
 })(jQuery, window);
