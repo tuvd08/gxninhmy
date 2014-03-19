f7bb4c2998c2c141d7727b6fff8157832c05e55c
tu-vu-duy <tuvd@exoplatform.com>
2014-03-19 14:13:11 +0700

    Update show post-content style

--- wp-content/themes/twentythirteen/content.php
+++ wp-content/themes/twentythirteen/content.php
@@ -18,61 +18,58 @@
       </td>
       <td class="main-column post-content">
 
-<div class="uiBox on-hide-menu" id="post-detail">
+        <div class="uiBox on-hide-menu" id="post-detail">
+          <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <header class="entry-header">
 
-    
-        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-          <header class="entry-header">
+              <?php if ( is_single() ) : ?>
+              <h1 class="entry-title"><?php the_title(); ?></h1>
+              <?php else : ?>
+              <h1 class="entry-title">
+                <a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
+              </h1>
+              <?php endif; // is_single() ?>
 
-            <?php if ( is_single() ) : ?>
-            <h1 class="entry-title"><?php the_title(); ?></h1>
-            <?php else : ?>
-            <h1 class="entry-title">
-              <a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
-            </h1>
-            <?php endif; // is_single() ?>
+              <div class="entry-meta">
+                <?php twentythirteen_entry_meta(); ?>
+                <?php edit_post_link( __( 'Edit', 'twentythirteen' ), '<span class="edit-link">', '</span>' ); ?>
+              </div><!-- .entry-meta -->
+            </header><!-- .entry-header -->
 
-            <div class="entry-meta">
-              <?php twentythirteen_entry_meta(); ?>
-              <?php edit_post_link( __( 'Edit', 'twentythirteen' ), '<span class="edit-link">', '</span>' ); ?>
-            </div><!-- .entry-meta -->
-          </header><!-- .entry-header -->
-
-          <?php if ( has_post_thumbnail() ) : ?>
-            <div class="entry-thumbnail">
-              <?php $full_img = wp_get_attachment_image_src(get_post_thumbnail_id(), 'full');  ?>
-              <a class="image-box" href="<?php echo $full_img[0]; ?>">
-                <?php the_post_thumbnail("large"); ?>
-              </a>
-            </div>
-          <?php endif; ?>
-
-          <?php if ( is_search() ) : // Only display Excerpts for Search ?>
-          <div class="entry-summary">
-            <?php the_excerpt(); ?>
-          </div><!-- .entry-summary -->
-          <?php else : ?>
-          <div class="entry-content">
-            <?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentythirteen' ) ); ?>
-            <?php wp_link_pages( array( 'before' => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentythirteen' ) . '</span>', 'after' => '</div>', 'link_before' => '<span>', 'link_after' => '</span>' ) ); ?>
-          </div><!-- .entry-content -->
-          <?php endif; ?>
-
-          <footer class="entry-meta">
-            <?php if ( comments_open() && ! is_single() ) : ?>
-              <div class="comments-link">
-                <?php comments_popup_link( '<span class="leave-reply">' . __( 'Leave a comment', 'twentythirteen' ) . '</span>', __( 'One comment so far', 'twentythirteen' ), __( 'View all % comments', 'twentythirteen' ) ); ?>
-              </div><!-- .comments-link -->
-            <?php endif; // comments_open() ?>
-
-            <?php if ( is_single() && get_the_author_meta( 'description' ) && is_multi_author() ) : ?>
-              <?php get_template_part( 'author-bio' ); ?>
+            <?php if ( has_post_thumbnail() ) : ?>
+              <div class="entry-thumbnail image-resize">
+                <?php $full_img = wp_get_attachment_image_src(get_post_thumbnail_id(), 'full');  ?>
+                <a class="image-box" href="<?php echo $full_img[0]; ?>">
+                  <?php the_post_thumbnail("large"); ?>
+                </a>
+              </div>
             <?php endif; ?>
-          </footer><!-- .entry-meta -->
-        </article><!-- #post -->
-</div>
 
-      
+            <?php if ( is_search() ) : // Only display Excerpts for Search ?>
+            <div class="entry-summary">
+              <?php the_excerpt(); ?>
+            </div><!-- .entry-summary -->
+            <?php else : ?>
+            <div class="entry-content image-resize">
+              <?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'twentythirteen' ) ); ?>
+              <?php wp_link_pages( array( 'before' => '<div class="page-links"><span class="page-links-title">' . __( 'Pages:', 'twentythirteen' ) . '</span>', 'after' => '</div>', 'link_before' => '<span>', 'link_after' => '</span>' ) ); ?>
+            </div><!-- .entry-content -->
+            <?php endif; ?>
+
+            <footer class="entry-meta">
+              <?php if ( comments_open() && ! is_single() ) : ?>
+                <div class="comments-link">
+                  <?php comments_popup_link( '<span class="leave-reply">' . __( 'Leave a comment', 'twentythirteen' ) . '</span>', __( 'One comment so far', 'twentythirteen' ), __( 'View all % comments', 'twentythirteen' ) ); ?>
+                </div><!-- .comments-link -->
+              <?php endif; // comments_open() ?>
+
+              <?php if ( is_single() && get_the_author_meta( 'description' ) && is_multi_author() ) : ?>
+                <?php get_template_part( 'author-bio' ); ?>
+              <?php endif; ?>
+            </footer><!-- .entry-meta -->
+          </article><!-- #post -->
+        </div>
+
       </td>
       <td class="right-column">
         <div class="min right-bar"></div>
--- wp-content/themes/twentythirteen/js/simple-js.js
+++ wp-content/themes/twentythirteen/js/simple-js.js
@@ -2,55 +2,118 @@
 
     window.Load = {};
     window.Resize = new Array(); //push
+    window.ResizeWidth = new Array();
+
+    Load.currWidth = 0;
+        
+    Load.animateWidth = function(settings) {
+      return {
+        run : function(elm) {
+          var jelm = $(elm);
+          if(settings.from && settings.from >= 0) {
+            jelm.css('width', from + 'px');
+          }
+          jelm.animate({'width': settings.to + 'px'}, (settings.time) ? settings.time : 300, function(){
+            if (typeof(settings.callback) == "function") {settings.callback(this) };
+          });
+          return jelm;
+        }
+      };
+    }
+    $.fn.animateWidth = function(settings) {
+      var outerArguments = arguments;
+      if(typeof settings === 'undefined') {
+        settings = {to:0};
+      }
+      return this.each(function() {
+        var instance = $.data(this, 'animateWidth') || $.data(this, 'animateWidth', new Load.animateWidth(settings));
+        return instance.run.call(this, this);
+      });
+    };
+  
+
+    Load.loadMin700 = function() {
+      window.maxWidth = $('body').width();
+      if(window.maxWidth < 700) {
+        $('body').addClass('min-body');
+        $('.max').hide();
+        $('.min').show();
+        $('.right-column').css({padding:'0px', margin: '0px', width : '5px'});
+        $('.left-column').css({padding:'0px', margin: '0px', width : '5px'});
+        $('.menu-container').css('position', 'relative')
+           .find('.main_menu').addClass('show-menu-block');
+        
+        $('.on-show-menu').on('click', function() {
+          $('.menu-container').toggle(500);
+        });
+        
+        $('.on-hide-menu').on('click', function() {
+          $('.menu-container').hide(500);
+        });
+     } else {
+       if($('.min').eq(0).css('display') === 'block') {
+         $('.max').show();
+         $('.min').hide();
+         //$('.right-column').attr('style', '');
+         $('.left-column').animateWidth({to: 250, callback:function(elm){ $(elm).attr('style', '');} }); 
+         $('.menu-container').attr('style', '')
+           .find('.main_menu').removeClass('show-menu-block');
+       }
+     }
+    }
     
+    Load.loadMin900 = function() {
+      window.maxWidth = $('body').width();
+      //right-container
+      if(window.maxWidth < 900) {
+        $('.right-column').css({padding:'0px', margin: '0px', width : '5px'});
+        $('.right-container').hide();
+      } else {
+       if($('.right-container').css('display') === 'none' || $('.right-column').width() < 20) {
+         $('.right-container').show();
+         $('.right-column').animateWidth({to: 280, callback:function(elm){ $(elm).attr('style', '');} }); 
+       }
+     }
+    }
     
-    
-    
+    Load.maxWidthImg = function() {
+      var parent = $('.image-resize');
+      if(parent.length > 0) {
+        //var imgs = parent.find('img');
+        //imgs.animate({'width': (parent.width() - 10) + 'px'}, 300, function(){$(this).css('width', 'auto');});
+      }
+    }
+
     Load.onload = function() {
       window.maxWidth = $('body').width();
-      
-      if(window.maxWidth < 600) {
-          $('body').addClass('min-body');
-          $('.max').hide();
-          $('.min').show();
-          $('.right-column').css({padding:'0px', margin: '0px', width : '5px'});
-          $('.left-column').css({padding:'0px', margin: '0px', width : '5px'});
-          $('.menu-container').css('position', 'relative')
-             .find('.main_menu').addClass('show-menu-block');
-          
-          $('.on-show-menu').on('click', function() {
-            $('.menu-container').toggle(500);
-          });
-          
-          $('.on-hide-menu').on('click', function() {
-            $('.menu-container').hide(500);
-          });
-       }
+
+      Load.loadMin700();
       //
-       $('.image-box').fancybox();
+      Load.loadMin900();
+      //
+      $('.image-box').fancybox();
        
-       var post = $('#post-detail');
-       if(post.length > 0) {
-          var postContent = post.find('.entry-content');
-          var maxW = postContent.width() - 10;
-          var imgs = postContent.find('img');
-          imgs.each(function(index) {
-              var img = $(this).css('max-width', maxW + 'px').css({'padding':'0px', 'margin': '10px 0px'}).addClass('thumbnail');
-              img.after( '<a class="img-' +index+ '" href="' + img.attr('src') + '"></a>');
-              var a = postContent.find('a.img-'+index + ':first');
-              a.append(img);
-              a.fancybox();
-          });
-       }
+      var post = $('#post-detail');
+      if(post.length > 0) {
+        var postContent = post.find('.entry-content');
+        var maxW = postContent.width() - 10;
+        var imgs = postContent.find('img');
+        imgs.each(function(index) {
+          var img = $(this).css({'padding':'0px', 'margin': '10px 0px'}).addClass('thumbnail');
+          img.after( '<a class="img-' +index+ '" href="' + img.attr('src') + '"></a>');
+          var a = postContent.find('a.img-'+index + ':first');
+          a.append(img);
+          a.fancybox();
+        });
+      }
        
-       $('.link-post-image').css('padding-top', function() {
-          var thizz = $(this);
-          if(thizz.height() >= 40) {
-            return 0;
-          }
-          return 15;
-       });
-       
+      $('.link-post-image').css('padding-top', function() {
+        var thizz = $(this);
+        if(thizz.height() >= 40) {
+          return 0;
+        }
+        return 15;
+      });
     }
 
     Load.getLayer = function() {
@@ -72,16 +135,28 @@
     });
 
     // resize
-//    window.Resize.push(Load.pdTop);
+    window.ResizeWidth.push(Load.loadMin700);
+    window.ResizeWidth.push(Load.loadMin900);
+    window.ResizeWidth.push(Load.maxWidthImg);
 
-    $(window).on('resize', function () {
+    $(window).on('resize', function (evt) {
+      try {
         for (var i = 0; i < window.Resize.length; ++i) {
-            window.Resize[i]();
+          var method = window.Resize[i];
+          if (typeof(method) == "function") method(evt) ;
         }
+      } catch(e) {};
+      
+      if (Load.currWidth != document.documentElement.clientWidth) {
+        try {
+          for (var i = 0; i < window.ResizeWidth.length; ++i) {
+            var method = window.ResizeWidth[i];
+            if (typeof(method) == "function") method(evt) ;
+          }
+        } catch(e) {};
+      }
+      Load.currWidth = document.documentElement.clientWidth;
     });
-    
-    
-    
     
     
   window.closeLayer = Load.closeLayer;
